c20c1069fd8e04a3164f6b1d02f27848a4ee0f7e
Pavan Kumar Reddy <67476441+pavandantu18@users.noreply.github.com>
2025-02-11 17:30:57 +0000

    code and jar files

--- src/main/java/com/example/WordMapper.java
+++ src/main/java/com/example/WordMapper.java
@@ -1,5 +1,20 @@
 package com.example;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class WordMapper{
-    
+import java.io.IOException;
+
+public class WordMapper extends Mapper<LongWritable,Text,Text,IntWritable>{
+    @Override
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+    String line = value.toString();
+    for (String word : line.split(" ")){
+        if(word.length()>0){
+            context.write(new Text(word),new IntWritable(1));
+        }
+    }
+}
 }
